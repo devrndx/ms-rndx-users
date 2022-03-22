9633b439cd011558934fc1872ccaaddd6b749929
devrndx <dev@round-x.com>
2022-03-22 12:08:16 +0900

    update user balance check

---
+++ migrations/sqls/20220322011001-sample-user-data-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+INSERT INTO `users` (`id`, `uuid`, `name`, `email`, `phonenum`, `wallet_address`, `balance`,`withdraw_limit`,`privateKey`, `created`, `last_updated`)
+VALUES
+	(6, '5fc0a65e-c67a-4a15-811e-bd24e8e7ef5f', 'test6', 'test@test.com', '01012341234', '0xF12cFe5A76eB70cEDf22DdD06FC22be91d38A0Ae',300,100,'','2017-03-20 01:28:20', '2017-04-18 01:30:58');
---
+++ migrations/sqls/20220322011947-users-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE `users` MODIFY `balance` varchar(1000) NOT NULL DEFAULT '';
