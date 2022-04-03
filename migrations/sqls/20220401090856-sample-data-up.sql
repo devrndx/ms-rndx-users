f6f035543dd4cb78487abd38967218c390ac7114
Dev <dev@round-x.com>
2022-04-03 19:35:10 +0900

    remove redanduncy samples

--- migrations/sqls/20220401090856-sample-data-up.sql
+++
@@ -1,7 +0,0 @@
-/* Replace with your SQL commands */
-INSERT INTO `users` (`id`, `uuid`, `name`, `email`, `phonenum`, `wallet_address`, `balance`,`withdraw_limit`,`privateKey`, `created`, `last_updated`)
-VALUES
-	(7, '5fc0a65e-c67a-4a15-811e-bd24e8e7ef5f', 'test6', 'test9@test.com', '01012341224', '0x5b7171534bd972951cf39ba93e49f88595e508ff',300,100,'','2017-03-20 01:28:20', '2017-04-18 01:30:58');
-	(8, '5fc0a65e-c67a-4a15-811e-bd24e8e7ef5f', 'test7', 'test10@test.com', '01012341334', '0xfef53d325656d035042c72c2b60e322f923dbc62',300,100,'','2017-03-20 01:28:20', '2017-04-18 01:30:58');
-	(9, '5fc0a65e-c67a-4a15-811e-bd24e8e7ef5f', 'test8', 'test11@test.com', '01012342234', '0x3dc7ec9ef47703663f28c31727c1068bcf19db37',300,100,'','2017-03-20 01:28:20', '2017-04-18 01:30:58');
-	(10, '5fc0a65e-c67a-4a15-811e-bd24e8e7ef5f', 'test9', 'test12@test.com', '01012345234', '0xb7edab3d97f6cf656fe989f4918c36bd30aa6662',300,100,'','2017-03-20 01:28:20', '2017-04-18 01:30:58');
