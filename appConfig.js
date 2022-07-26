0d4b7f68173e1eb9169a76c2987c615b26436990
Dev <dev@round-x.com>
2022-07-26 19:31:45 +0900

    update transaction service

--- appConfig.js
+++ appConfig.js
@@ -10,92 +10,94 @@ require("app-module-path").addPath(path.join(__dirname, "/lib"));
 
 // Add all routes and route-handlers for your service/app here:
 function serviceRoutes(app) {
-    // For Liveness Probe, defaults may be all you need.
-    const livenessCheck = healthcheck({ path: "/ping" });
-    app.use(livenessCheck.express());
+  // For Liveness Probe, defaults may be all you need.
+  const livenessCheck = healthcheck({ path: "/ping" });
+  app.use(livenessCheck.express());
 
-    // For readiness check, let's also test the DB
-    const check = healthcheck();
-    const AdvancedHealthcheckers = require("healthchecks-advanced");
-    const advCheckers = new AdvancedHealthcheckers();
-    // Database health check is cached for 10000ms = 10 seconds!
-    check.addCheck("db", "dbQuery", advCheckers.dbCheck, { minCacheMs: 10000 });
-    app.use(check.express());
+  // For readiness check, let's also test the DB
+  const check = healthcheck();
+  const AdvancedHealthcheckers = require("healthchecks-advanced");
+  const advCheckers = new AdvancedHealthcheckers();
+  // Database health check is cached for 10000ms = 10 seconds!
+  check.addCheck("db", "dbQuery", advCheckers.dbCheck, { minCacheMs: 10000 });
+  app.use(check.express());
 
-    /* eslint-disable global-require */
+  /* eslint-disable global-require */
 
-    // Temporary allow all urls
-    const safesitelist =
-        process.env.NODE_ENV == "production" ? [
-            "https://rndx-wallet.io",
-            "https://app.rndx-wallet.io",
-            "https://admin.rndx-wallet.io",
-        ] : [
-            "http://192.168.0.7:34129",
-            "https://dev.rndx-wallet.io",
-            "https://app.dev.rndx-wallet.io",
-            "https://admin.dev.rndx-wallet.io",
+  // Temporary allow all urls
+  const safesitelist =
+    process.env.NODE_ENV == "production"
+      ? [
+          "https://rndx-wallet.io",
+          "https://app.rndx-wallet.io",
+          "https://admin.rndx-wallet.io",
+        ]
+      : [
+          "http://192.168.0.7:34129",
+          "https://dev.rndx-wallet.io",
+          "https://app.dev.rndx-wallet.io",
+          "https://admin.dev.rndx-wallet.io",
         ];
 
-    const corsOptions = {
-        origin: function(origin, callback) {
-            const issafesitelisted = safesitelist.indexOf(origin) !== -1;
-            callback(null, issafesitelisted);
-        },
-        credentials: true,
-    };
+  const corsOptions = {
+    origin: function (origin, callback) {
+      const issafesitelisted = safesitelist.indexOf(origin) !== -1;
+      callback(null, issafesitelisted);
+    },
+    credentials: true,
+  };
 
-    app.use(cors(corsOptions));
-    app.use("/users", require("users")); // attach to sub-route
-    /* eslint-enable global-require */
+  app.use(cors(corsOptions));
+  app.use("/users", require("users")); // attach to sub-route
+  /* eslint-enable global-require */
 }
 
 function setupErrorHandling(app) {
-    // Custom formatting for error responses.
-    app.use((err, req, res, next) => {
-        if (err) {
-            const out = {};
-            if (err.isJoi || err.type === "validation") {
-                //validation error. No need to log these
-                out.errors = err.details;
-                res.status(400).json(out);
-                return;
-            } else {
-                log.error(err);
-                if (process.env.NODE_ENV === "production") {
-                    out.errors = ["Internal server error"];
-                } else {
-                    out.errors = [err.toString()];
-                }
-                res.status(500).json(out);
-                return;
-            }
+  // Custom formatting for error responses.
+  app.use((err, req, res, next) => {
+    if (err) {
+      const out = {};
+      if (err.isJoi || err.type === "validation") {
+        //validation error. No need to log these
+        out.errors = err.details;
+        res.status(400).json(out);
+        return;
+      } else {
+        log.error(err);
+        if (process.env.NODE_ENV === "production") {
+          out.errors = ["Internal server error"];
+        } else {
+          out.errors = [err.toString()];
         }
-        return next();
-    });
+        res.status(500).json(out);
+        return;
+      }
+    }
+    return next();
+  });
 }
 
-exports.setup = function(app, callback) {
-    // Choose your favorite view engine(s)
-    app.set("view engine", "handlebars");
-    app.engine("handlebars", hbs.__express);
+exports.setup = function (app, callback) {
+  // Choose your favorite view engine(s)
+  app.set("view engine", "handlebars");
+  app.engine("handlebars", hbs.__express);
 
-    /** Adding security best-practices middleware
-     * see: https://www.npmjs.com/package/helmet **/
-    app.use(helmet());
+  /** Adding security best-practices middleware
+   * see: https://www.npmjs.com/package/helmet **/
+  app.use(helmet());
 
-    //---- Mounting well-encapsulated application modules (so-called: "mini-apps")
-    //---- See: http://expressjs.com/guide/routing.html and http://vimeo.com/56166857
-    serviceRoutes(app);
+  //---- Mounting well-encapsulated application modules (so-called: "mini-apps")
+  //---- See: http://expressjs.com/guide/routing.html and http://vimeo.com/56166857
+  serviceRoutes(app);
 
-    setupErrorHandling(app);
+  setupErrorHandling(app);
 
-    // If you need websockets:
-    // let socketio = require('socket.io')(runningApp.http);
-    // require('fauxchatapp')(socketio);
+  // If you need websockets:
+  // let socketio = require('socket.io')(runningApp.http);
+  // require('fauxchatapp')(socketio);
 
-    if (typeof callback === "function") {
-        callback(app);
-        return;
-    }
+  if (typeof callback === "function") {
+    callback(app);
+    return;
+  }
 };
---
+++ migrations/sqls/20220726082259-txInfo-up.sql
@@ -0,0 +1,13 @@
+/* Replace with your SQL commands */
+CREATE TABLE `txInfo` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `sWalletAddress` varchar(1000) NOT NULL DEFAULT '',
+    `sFromAddress` varchar(1000) NOT NULL DEFAULT '',
+    `sToAddr` varchar(1000) NOT NULL DEFAULT '',
+    `nAmount` varchar(1000) NOT NULL DEFAULT '0',
+    `sTxHash` varchar(1000) NOT NULL DEFAULT '',
+    `sTimeStamp` varchar(100) NOT NULL DEFAULT '',
+    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_updated` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
